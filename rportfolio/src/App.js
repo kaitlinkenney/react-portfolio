cc62bf003fddc8fcf399134f527b18cae26ae64e
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-24 13:48:45 -0500

    header and footer working

---
+++ rportfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ rportfolio/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>
+      <a href={'http://google.com'}>Google</a>
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
