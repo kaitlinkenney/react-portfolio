57e3dd537fd7f1072ce0b99775f74f127f401d97
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-28 10:44:50 -0500

    footer organized

--- rportfolio/src/components/Footer/footer.js
+++ rportfolio/src/components/Footer/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { GrGithub } from "react-icons/gr";
+import { GrGithub, GrMail } from "react-icons/gr";
 import { GrLinkedin } from "react-icons/gr";
 
 function Footer() {
@@ -8,13 +8,10 @@ function Footer() {
     <footer className="footer mt-36">
       <div>
       <span>
-          <p className="p mt-2.5">< a href="mailto:kaitlin.kenney96@gmail.com">kaitlin.kenney96@gmail.com</a> </p>
           
-          <a className="icon items-center mr-2.5" style={{float: "right"}} href={'https://github.com/kaitlinkenney'}><GrGithub className="-pb-6" style={{ height: 39, width: 35}} /></a>
-          
-      
-          <a className="iconn pl-2" style={{float: "right"}} href={'https://www.linkedin.com/in/kaitlin-kenney-12b01a1a7/'}><GrLinkedin className="mr-6 -pb-6" style={{ height: 40, width: 30 }}/></a>
-  
+          <a className="icon mr-2.5" style={{float: "right"}} href={'https://github.com/kaitlinkenney'}><GrGithub className="-pb-6" style={{ height: 39, width: 35}} /></a>
+          <a className="iconn pl-2" style={{float: "right"}} href={'https://www.linkedin.com/in/kaitlin-kenney-12b01a1a7/'}><GrLinkedin className="mr-2 -pb-6" style={{ height: 40, width: 30 }}/></a>
+          <a className="iconnn" style={{float: "right"}} href={"mailto:kaitlin.kenney96@gmail.com"}><GrMail className="pb-2" style={{height: 50, width: 35}}/></a>
       </span>
       </div>
     </footer>
