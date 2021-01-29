be57471a38256af50b257709e24f6ce909e96f3b
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-29 10:37:48 -0500

    about page half fixed

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -9,9 +9,9 @@ function Footer() {
       <div>
       <span>
           
-          <a className="icon mr-2.5" style={{float: "right"}} href={'https://github.com/kaitlinkenney'}><GrGithub className="-pb-6" style={{ height: 39, width: 35}} /></a>
-          <a className="iconn pl-2" style={{float: "right"}} href={'https://www.linkedin.com/in/kaitlin-kenney-12b01a1a7/'}><GrLinkedin className="mr-2 -pb-6" style={{ height: 40, width: 30 }}/></a>
-          <a className="iconnn" style={{float: "right"}} href={"mailto:kaitlin.kenney96@gmail.com"}><GrMail className="pb-2" style={{height: 50, width: 35}}/></a>
+          <a className="github" style={{float: "right"}} href={'https://github.com/kaitlinkenney'}><GrGithub style={{ height: 39, width: 35}} /></a>
+          <a className="linkedin" style={{float: "right"}} href={'https://www.linkedin.com/in/kaitlin-kenney-12b01a1a7/'}><GrLinkedin style={{ height: 40, width: 30 }}/></a>
+          <a className="email" style={{float: "right"}} href={"mailto:kaitlin.kenney96@gmail.com"}><GrMail style={{height: 50, width: 35}}/></a>
       </span>
       </div>
     </footer>
--- src/pages/about.js
+++ src/pages/about.js
@@ -8,11 +8,11 @@ import "./about.css";
 function About() {
     return (
 
-        <div className="container pt-4 pb-12 mt-12 opacity-80 pr-4 pl-4" style={{ background: "#215" }}>
+        <div className="container containerone" style={{ background: "#215" }}>
             <Image />
-            <div className="container shadow-lg bg-gray-200 pt-3 mt-3">
-                <h1 className="skills pt-2"> Skills Overview </h1>
-                <p className="pt-3 container">  I am a full stack web developer originally from Cape May, New Jersey.
+            <div className=" container containertwo">
+                <h1 className="skills"> Skills Overview </h1>
+                <p className="container containerthree">  I am a full stack web developer originally from Cape May, New Jersey.
                 I currently teach ESL (English as a second language) online in my spare time.
                 I obtained my certificate in web development from Rutgers University Full Stack Web Development Bootcamp.
                 Previously, I have attended Rutgers University with a major in psychology and Camden County College, 
@@ -20,52 +20,52 @@ function About() {
                 I have experience in various different customer service positions that I have worked in
                 throughout school. I like to problem solve and I hope to continuously build on my knowledge and
                 skill set. </p>
-                <p className="pb-4 pt-1 resume"> <a href="https://docs.google.com/document/d/1wnHaEFWsO2BqsGK51IQHHZROhXPmVo6s2M1gN6VSUFE/edit">View my resume here!</a>
+                <p className="resume"> <a href="https://docs.google.com/document/d/1wnHaEFWsO2BqsGK51IQHHZROhXPmVo6s2M1gN6VSUFE/edit">View my resume here!</a>
                 </p>
             </div>
             <div className="row">
-                <div className="container shadow-lg ml-12 mt-4 pb-3 pt-3 bg-gray-200" style={{ width: "500px" }}>
-                    <HiOutlineDesktopComputer className="mx-auto" style={{ height: 35, width: 35 }} />
-                    <div className="row justify-center">
+                <div className="container containerfour" style={{ width: "500px" }}>
+                    <HiOutlineDesktopComputer className="desktop" style={{ height: 35, width: 35 }} />
+                    <div className="rows">
                         <h3> Front End</h3>
                     </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> HTML5
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> CSS3
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> Bootstrap
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> Materialize
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> TailwindCss
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> React
                         </div>
                 </div>
-                <div className="container shadow-lg mt-4 pt-3 bg-gray-200" style={{ width: "500px" }}>
-                    <DiStreamline className="mx-auto" style={{ height: 35, width: 35 }} />
-                    <div className="row justify-center">
+                <div className="container containerfive" style={{ width: "500px" }}>
+                    <DiStreamline className="db" style={{ height: 35, width: 35 }} />
+                    <div className="rows">
                         <h3> Back End</h3>
                     </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> Node
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> Express
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> MySQL
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> Sequelize
                         </div>
-                    <div className="row justify-center">
+                    <div className="rows">
                         < BiBadgeCheck style={{ height: 25 }} /> MongoDB
                         </div>
                 </div>
