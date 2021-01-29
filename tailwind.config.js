5d4a45600b0c88c48d299f88b3e1b9391e5eaee5
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-28 21:17:12 -0500

    updated components in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
     './src/components/Footer/footer.js',
     './src/components/Header/header.js',
     './src/components/Project/project.js',
-    './pages/about.js',
-    './pages.home.js',
-    './pagse.projects.js',
+    './src/pages/about.js',
+    './src/pages.home.js',
+    './src/pages.projects.js',
+    './src/App.js',
     './public/index.html'
   ],
   darkMode: false, // or 'media' or 'class'
